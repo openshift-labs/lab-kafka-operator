fb102a5a8ea10bca345d90cc97404436dc42eb9f
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-10-25 19:25:34 +1100

    Reconfiguration for latest spawner and workshop scripts.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-kafka-operator
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-kafka-operator:1.4
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-kafka-operator:latest
 WORKSHOP_TITLE="Using the Kafka Operator"
 WORKSHOP_DESCRIPTION="A workshop on using the Kafka operator"
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.1
